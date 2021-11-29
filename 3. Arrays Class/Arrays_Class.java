a012956d9927cb99a1ee06f0403bdc47d20811aa
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-11-29 20:16:50 +0530

    Create Arrays_Class.java

---
+++ 3. Arrays Class/Arrays_Class.java
@@ -0,0 +1,72 @@
+package utilityClass;
+
+import java.util.Arrays;
+
+public class Arrays_Class 
+{
+
+	public static void main(String[] args) 
+	{
+		int[] a = {5,10,8,7,13};
+		System.out.println("Before Sort");
+		for(int no:a)
+		{
+			System.out.print(no + " ");
+		}
+		
+		System.out.println();
+		Arrays.sort(a);
+		
+		//for each loop
+		System.out.println("After Sort");
+		for(int no:a)
+		{
+			System.out.print(no +" ");
+		}
+
+		
+		System.out.println();
+		System.out.println();
+		
+		
+		
+		
+		System.out.println("Before Sort");
+		
+		String[] s = {"a","ab","abc","c","b","cb","bca"};
+		for(String no:s)
+		{
+			System.out.print(no + " ");
+		}
+		System.out.println();
+		Arrays.sort(s);
+		
+		//for each loop
+		System.out.println("After Sort");
+		for(String no:s)
+		{
+			System.out.print(no +" ");
+		}
+		
+		
+		
+	}
+
+}
+
+/*
+
+output:-
+
+Before Sort
+5 10 8 7 13 
+After Sort
+5 7 8 10 13 
+
+Before Sort
+a ab abc c b cb bca 
+After Sort
+a ab abc b bca c cb 
+
+
+*/
