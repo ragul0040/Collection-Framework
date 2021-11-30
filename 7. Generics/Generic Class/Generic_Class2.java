4ac065bd40041aa59c9e421e65c1a50bf8a373c5
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-11-30 08:14:29 +0530

    Create Generic_Class2.java

---
+++ 7. Generics/Generic Class/Generic_Class2.java
@@ -0,0 +1,26 @@
+package generic;
+
+public class Generic_Class2
+{
+
+	public static void main(String[] args) 
+	{
+		Generic_Class g=new Generic_Class("abcd");
+		g.display();
+		
+		Generic_Class g1=new Generic_Class(123);
+		g1.display();
+		
+
+	}
+
+}
+
+
+/*
+
+
+abcd java.lang.String
+123 java.lang.Integer
+
+*/
